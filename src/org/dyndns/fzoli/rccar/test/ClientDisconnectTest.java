68614567175dddc616ad70f5a6deb426b782d2bd
zoli <zoli@NB-FZoli>
2012-10-04 12:43:56 +0200

    note

--- src/org/dyndns/fzoli/rccar/test/ClientDisconnectTest.java
+++ src/org/dyndns/fzoli/rccar/test/ClientDisconnectTest.java
@@ -14,7 +14,7 @@ import org.dyndns.fzoli.socket.process.impl.ClientDisconnectProcess;
 public class ClientDisconnectTest {
     
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i <= 1; i++) { // két kapcsolatot fog kialakítani
+        for (int i = 0; i <= 1; i++) { // két kapcsolatot fog kialakítani. élesben is hasonló lesz, annyi eltéréssel, hogy az első kapcsolat kiépítését be fogja várni és aztán épül ki a többi, ha az sikerült
             SSLSocket s = SSLSocketUtil.createClientSocket("192.168.20.5", 8443, new File("test-certs/ca.crt"), new File("test-certs/controller.crt"), new File("test-certs/controller.key"), new char[]{});
             new Thread(new AbstractSecureClientHandler(s, 5, i) {  // az eszközazonosító 5, a kapcsolatazonosító ciklusonként más
 

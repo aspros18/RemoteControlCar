7d38bd61d657f7fcc6b6daa15eb44984e2164f39
zoli <zoli@NB-FZoli>
2012-10-04 16:00:16 +0200

    testing

--- src/org/dyndns/fzoli/rccar/test/ClientDisconnectTest.java
+++ src/org/dyndns/fzoli/rccar/test/ClientDisconnectTest.java
@@ -1,6 +1,8 @@
 package org.dyndns.fzoli.rccar.test;
 
 import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import javax.net.ssl.SSLSocket;
 import org.dyndns.fzoli.socket.SSLSocketUtil;
 import org.dyndns.fzoli.socket.handler.AbstractSecureClientHandler;
@@ -43,17 +45,29 @@ public class ClientDisconnectTest {
     }
     
     /**
-     * Kapcsolódik a szerverhez a megadott kapcsolatazonosítóval.
+     * Kapcsolódik a szerverhez teszt célból.
      */
-    private static void connect(int connId) throws Exception {
-        SSLSocket s = SSLSocketUtil.createClientSocket("192.168.20.5", 8443, new File("test-certs/ca.crt"), new File("test-certs/controller.crt"), new File("test-certs/controller.key"), new char[]{});
-        new Thread(new TestClientHandler(s, 5, connId)).start(); // új szálban indítás; eszközazonosító: 5; kapcsolatazonosító a paraméterben átadott érték
+    private static SSLSocket createSocket() throws GeneralSecurityException, IOException {
+        return SSLSocketUtil.createClientSocket("192.168.20.5", 8443, new File("test-certs/ca.crt"), new File("test-certs/controller.crt"), new File("test-certs/controller.key"), new char[]{});
     }
     
     public static void main(String[] args) throws Exception {
-        for (int i = 0; i <= 3; i++) { // négy kapcsolatot fog kialakítani. élesben is hasonló lesz, annyi eltéréssel, hogy az első kapcsolat kiépítését be fogja várni és aztán épül ki a többi, ha az sikerült
-            connect(i); //kapcsolódás a ciklusváltozót használva kapcsolatazonosítónak
-        }
+        // az első kapcsolódás a szerverhez új szálban
+        new Thread(new TestClientHandler(createSocket(), 5, 0) { // eszközazonosító: 5; kapcsolatazonosító: 0
+
+            @Override
+            protected void onProcessSelected() { // ha sikerült az első kapcsolódás
+                try {
+                    for (int i = 1; i <= 3; i++) { // további három tesztkapcsolat kialakítása; remélhetőleg sikeres lesz, mivel az első kapcsolódás sikerült
+                        new Thread(new TestClientHandler(createSocket(), 5, i) {}).start(); // új szálban indítás; eszközazonosító: 5; kapcsolatazonosítók: 1 - 3
+                    }
+                }
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            
+        }).start();
     }
     
 }

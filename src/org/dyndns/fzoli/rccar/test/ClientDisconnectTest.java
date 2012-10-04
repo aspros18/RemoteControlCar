8f7c3e5167b9ed5e05de9ebbd97220195c0af475
zoli <zoli@NB-FZoli>
2012-10-04 17:17:11 +0200

    testing

--- src/org/dyndns/fzoli/rccar/test/ClientDisconnectTest.java
+++ src/org/dyndns/fzoli/rccar/test/ClientDisconnectTest.java
@@ -6,9 +6,6 @@ import java.security.GeneralSecurityException;
 import javax.net.ssl.SSLSocket;
 import org.dyndns.fzoli.socket.SSLSocketUtil;
 import org.dyndns.fzoli.socket.handler.AbstractSecureClientHandler;
-import org.dyndns.fzoli.socket.handler.Handler;
-import org.dyndns.fzoli.socket.handler.HandlerException;
-import org.dyndns.fzoli.socket.handler.event.HandlerListener;
 import org.dyndns.fzoli.socket.process.AbstractSecureProcess;
 import org.dyndns.fzoli.socket.process.impl.ClientDisconnectProcess;
 
@@ -53,47 +50,20 @@ public class ClientDisconnectTest {
         
     }
     
-    /**
-     * Kapcsolódik a szerverhez teszt célból.
-     */
-    private static SSLSocket createSocket() throws GeneralSecurityException, IOException {
-        return SSLSocketUtil.createClientSocket("192.168.20.5", 8443, new File("test-certs/ca.crt"), new File("test-certs/controller.crt"), new File("test-certs/controller.key"), new char[]{});
-    }
-    
     public static void main(String[] args) throws Exception {
         new ClientConnectingTest(5, new int[] {0, 1, 2, 3}) {
 
             @Override
             protected SSLSocket createConnection() throws GeneralSecurityException, IOException {
-                return createSocket();
+                return SSLSocketUtil.createClientSocket("192.168.20.5", 8443, new File("test-certs/ca.crt"), new File("test-certs/controller.crt"), new File("test-certs/controller.key"), new char[]{});
             }
 
             @Override
             protected AbstractSecureClientHandler createHandler(SSLSocket socket, int deviceId, int connectionId) {
                 return new TestClientHandler(socket, deviceId, connectionId);
             }
+            
         }.connect();
-        
-        // az első kapcsolódás a szerverhez új szálban
-//        new Thread(new TestClientHandler(createSocket(), 5, 0) { // eszközazonosító: 5; kapcsolatazonosító: 0
-//            {
-//                addHandlerListener(new HandlerListener() {
-//                    
-//                    @Override
-//                    public void onProcessSelected(Handler handler) { // ha sikerült az első kapcsolódás
-//                        try {
-//                            for (int i = 1; i <= 3; i++) { // további három tesztkapcsolat kialakítása; remélhetőleg sikeres lesz, mivel az első kapcsolódás sikerült
-//                                new Thread(new TestClientHandler(createSocket(), 5, i) {}).start(); // új szálban indítás; eszközazonosító: 5; kapcsolatazonosítók: 1 - 3
-//                            }
-//                        }
-//                        catch (Exception ex) {
-//                            ex.printStackTrace();
-//                        }
-//                    }
-//                    
-//                });
-//            }
-//        }).start();
     }
     
 }

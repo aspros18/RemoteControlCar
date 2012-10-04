2db831b4b51b1f7fbd68dd9c195c228b520a9de9
zoli <zoli@NB-FZoli>
2012-10-04 17:30:52 +0200

    testing

--- src/org/dyndns/fzoli/rccar/test/ClientConnectingTest.java
+++ src/org/dyndns/fzoli/rccar/test/ClientConnectingTest.java
@@ -8,14 +8,25 @@ import org.dyndns.fzoli.socket.handler.Handler;
 import org.dyndns.fzoli.socket.handler.event.HandlerListener;
 
 /**
- *
+ * Kliens oldalra egyszerű kapcsolódást megvalósító osztály.
  * @author zoli
  */
 public abstract class ClientConnectingTest {
 
+    /**
+     * Eszközazonosító.
+     */
     private final int deviceId;
+    
+    /**
+     * Kapcsolatazonosítók.
+     */
     private final int[] connectionIds;
 
+    /**
+     * Eseménykezelő, ami lefut, ha sikerült az első kapcsolódás.
+     * Ha az első kapcsolódás sikerült, létrehozza a többi kapcsolatot is.
+     */
     private final HandlerListener listener = new HandlerListener() {
 
         @Override
@@ -29,16 +40,45 @@ public abstract class ClientConnectingTest {
         
     };
     
+    /**
+     * Egyszerű kapcsolódást megvalósító osztály konstruktora.
+     * @param deviceId eszközazonosító
+     * @param connectionIds kapcsolatazonosítókat tartalmazó tömb
+     */
     public ClientConnectingTest(int deviceId, int[] connectionIds) {
         if (connectionIds == null || connectionIds.length < 1) throw new IllegalArgumentException("At least one Connection ID needs to be added");
         this.deviceId = deviceId;
         this.connectionIds = connectionIds;
     }
     
+    /**
+     * Socket létrehozása.
+     * Kapcsolódás a szerverhez.
+     */
     protected abstract SSLSocket createConnection() throws GeneralSecurityException, IOException;
     
+    /**
+     * Kliens oldali Handler példányosítása.
+     * @param socket a kapcsolat a szerverrel
+     * @param deviceId az eszközazonosító
+     * @param connectionId a kapcsolatazonosító
+     */
     protected abstract AbstractSecureClientHandler createHandler(SSLSocket socket, int deviceId, int connectionId);
     
+    /**
+     * Ha kivétel keletkezik, ebben a metódusban le lehet kezelni.
+     * @param ex a keletkezett kivétel
+     * @param connectionId a közben használt kapcsolatazonosító
+     */
+    protected void onException(Exception ex, int connectionId) {
+        ;
+    }
+    
+    /**
+     * Kapcsolódik a szerverhez a megadott kapcsolatazonosítóval.
+     * @param connectionId a kapcsolatazonosító
+     * @param addListener megadja, kell-e eseményt hozzáadni
+     */
     private void runHandler(int connectionId, boolean addListener) {
         try {
             AbstractSecureClientHandler handler = createHandler(createConnection(), deviceId, connectionId);
@@ -46,10 +86,13 @@ public abstract class ClientConnectingTest {
             new Thread(handler).start();
         }
         catch (Exception ex) {
-            ex.printStackTrace();
+            onException(ex, connectionId);
         }
     }
     
+    /**
+     * Kapcsolódás a szerverhez.
+     */
     public void connect() {
         runHandler(connectionIds[0], true);
     }

222ca5fa65f020501f32e855f5f47aa1e7f0f3d7
zoli <zoli@NB-FZoli>
2012-10-07 23:43:43 +0200

    conf editor testing

--- src/org/dyndns/fzoli/rccar/controller/ConfigHelpDialog.java
+++ src/org/dyndns/fzoli/rccar/controller/ConfigHelpDialog.java
@@ -15,24 +15,24 @@ import javax.swing.ScrollPaneConstants;
  */
 public class ConfigHelpDialog extends JDialog {
 
-    private static final JLabel taHelp = new JLabel("<html>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>Donec consectetur vulputate quam, interdum laoreet<br>nunc elementum vel. Donec eget pharetra dolor.<br><br>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br>Donec consectetur vulputate quam, interdum laoreet<br>nunc elementum vel. Donec eget pharetra dolor.</html>");
+    private static final JLabel taHelp = new JLabel("<html>Ahhoz, hogy kapcsolódni lehessen a szerverhez,<br>meg kell adni az elérési útvonalát, ami a címből<br>és a portból áll. A cím lehet IP cím és domain is.<br><br>A kommunikáció titkosított kapcsolaton kereszül<br>folyik, ezért a kliensnek szüksége van tanúsítványra.<br><br>A tanúsítvány három fájlból tevődik össze:<br>- kiállító<br>- nyílvános kulcs<br>- titkos kulcs<br><br>Mindhárom fájl szükséges ahhoz, hogy a kapcsolat<br>létrejöhessen a szerverrel, ezért addig nem indítható<br>a program, míg nincs mind a három fájl beállítva.</html>");
     
     public ConfigHelpDialog(Dialog owner) {
-        super(owner, "Súgó");
-        setDefaultCloseOperation(HIDE_ON_CLOSE);
-        setModalityType(ModalityType.APPLICATION_MODAL);
+        super(owner, "Súgó"); // szülő és címsor szöveg beállítása
+        setDefaultCloseOperation(HIDE_ON_CLOSE); // bezáráskor elrejtődés
+        setModalityType(ModalityType.APPLICATION_MODAL); // modális dialógus
         
-        taHelp.setOpaque(true);
-        taHelp.setBackground(Color.WHITE);
-        taHelp.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        taHelp.setOpaque(true); // ne legyen átlátszó
+        taHelp.setBackground(Color.WHITE); // fehér háttérszín
+        taHelp.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5)); // 5x5-ös margó
         
-        JScrollPane sp = new JScrollPane(taHelp);
-        sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        add(sp);
+        JScrollPane sp = new JScrollPane(taHelp); // a szöveg scrollozható ...
+        sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER); // ... de csak vertikálisan
+        add(sp); // scrollpane hozzáadása az ablakhoz
         
-        pack();
-        setMinimumSize(new Dimension(getSize().width + 30, 1));
-        setLocationRelativeTo(owner);
+        pack(); // minimális méret beállítása és ...
+        setMinimumSize(new Dimension(getSize().width + 30, 1)); // ... az ablakmagasság csak ennél nagyobb lehet
+        setLocationRelativeTo(owner); // szülő ablak szerint középre igazítás
     }
     
 }

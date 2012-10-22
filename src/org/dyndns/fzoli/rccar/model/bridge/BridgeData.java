c0bd840926e7057eda6f01552411b87b2b1510ab
zoli <zoli@NB-FZoli>
2012-10-22 17:47:00 +0200

    model change

--- src/org/dyndns/fzoli/rccar/model/bridge/BridgeData.java
+++ src/org/dyndns/fzoli/rccar/model/bridge/BridgeData.java
@@ -3,6 +3,7 @@ package org.dyndns.fzoli.rccar.model.bridge;
 import java.util.ArrayList;
 import java.util.List;
 import org.dyndns.fzoli.rccar.model.controller.ChatMessageInfo;
+import org.dyndns.fzoli.rccar.model.controller.ControllerData;
 import org.dyndns.fzoli.rccar.model.host.HostData;
 
 /**
@@ -11,12 +12,35 @@ import org.dyndns.fzoli.rccar.model.host.HostData;
  */
 public class BridgeData {
     
-    private final String hostName;
-    private final HostData hostData = new HostData();
+    private final String HOST_NAME;
+    private final HostData HOST_DATA = new HostData();
     private final List<ChatMessageInfo> CHAT_MESSAGES = new ArrayList<ChatMessageInfo>();
 
     public BridgeData(String hostName) {
-        this.hostName = hostName;
+        HOST_NAME = hostName;
+    }
+
+    public String getHostName() {
+        return HOST_NAME;
+    }
+
+    public List<ChatMessageInfo> getChatMessages() {
+        synchronized(CHAT_MESSAGES) {
+            return new ArrayList<ChatMessageInfo>(CHAT_MESSAGES);
+        }
+    }
+    
+    public void addChatMessage(ChatMessageInfo m) {
+        if (m != null) synchronized(CHAT_MESSAGES) {
+            CHAT_MESSAGES.add(m);
+        }
+    }
+    
+    public ControllerData createControllerData() {
+        ControllerData d = new ControllerData();
+        d.setBatteryLevel(HOST_DATA.getBatteryLevel());
+        d.setGpsPosition(HOST_DATA.getGpsPosition());
+        return d;
     }
     
 }

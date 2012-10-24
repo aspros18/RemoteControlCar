4fc54b15e5e62ce8390fe2bfa40b1ba16d503801
zoli <zoli@NB-FZoli>
2012-10-24 23:18:07 +0200

    controller testing

--- src/org/dyndns/fzoli/rccar/controller/socket/ControllerMessageProcess.java
+++ src/org/dyndns/fzoli/rccar/controller/socket/ControllerMessageProcess.java
@@ -2,6 +2,8 @@ package org.dyndns.fzoli.rccar.controller.socket;
 
 import static org.dyndns.fzoli.rccar.controller.Main.showHostSelectionFrame;
 import static org.dyndns.fzoli.rccar.controller.Main.updateHostSelectionFrame;
+import org.dyndns.fzoli.rccar.model.PartialBaseData;
+import org.dyndns.fzoli.rccar.model.controller.ControllerData;
 import org.dyndns.fzoli.rccar.model.controller.HostList;
 import org.dyndns.fzoli.socket.handler.SecureHandler;
 import org.dyndns.fzoli.socket.process.impl.MessageProcess;
@@ -21,8 +23,14 @@ public class ControllerMessageProcess extends MessageProcess {
         if (o instanceof HostList) {
             showHostSelectionFrame((HostList) o);
         }
-        if (o instanceof HostList.PartialHostList) {
+        else if (o instanceof HostList.PartialHostList) {
             updateHostSelectionFrame((HostList.PartialHostList) o);
+        }
+        else if (o instanceof ControllerData) {
+            //TODO
+        }
+        else if (o instanceof PartialBaseData) {
+            //TODO
         }
     }
     

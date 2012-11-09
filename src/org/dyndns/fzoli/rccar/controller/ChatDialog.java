692a66e12a067722d848015cf919e88056e59ffc
zoli <zoli@NB-FZoli>
2012-11-09 17:02:14 +0100

    testing

--- src/org/dyndns/fzoli/rccar/controller/ChatDialog.java
+++ src/org/dyndns/fzoli/rccar/controller/ChatDialog.java
@@ -1,14 +1,22 @@
 package org.dyndns.fzoli.rccar.controller;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Window;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import static org.dyndns.fzoli.rccar.controller.ControllerWindows.IC_CHAT;
 import org.dyndns.fzoli.rccar.controller.ControllerWindows.WindowType;
+import org.dyndns.fzoli.ui.UIUtil;
 
 /**
  * Chatablak.
@@ -19,25 +27,62 @@ public class ChatDialog extends AbstractDialog {
     /**
      * A vezérlők listája.
      */
-    private final JList<String> LIST_CONTROLLERS = new JList<String>(new DefaultListModel<String>());
+    private final JList<String> LIST_CONTROLLERS = new JList<String>(new DefaultListModel<String>()) {
+        {
+            ((DefaultListModel)getModel()).addElement("controller"); // TODO: tesztsor
+            setCellRenderer(new DefaultListCellRenderer() {
+
+                /**
+                 * A lista elemei látszólag nem választhatóak ki és nem soha nincs rajtuk fókusz.
+                 */
+                @Override
+                public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                    return super.getListCellRendererComponent(list, value, index, false, false);
+                }
+                
+            });
+        }
+        
+    };
     
     /**
      * Az üzenetkijelző és üzenetküldő panel.
      */
-    private final JPanel PANEL_MESSAGES = new JPanel();
+    private final JPanel PANEL_CONTROLLERS = new JPanel() {
+        {
+            setLayout(new BorderLayout());
+            setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEtchedBorder(), BorderFactory.createLineBorder(LIST_CONTROLLERS.getBackground(), 4)));
+            JLabel lb = new JLabel("Jelenlévők");
+            lb.setOpaque(true);
+            lb.setBackground(LIST_CONTROLLERS.getBackground());
+            lb.setFont(new Font(lb.getFont().getFontName(), Font.BOLD, lb.getFont().getSize()));
+            add(lb, BorderLayout.NORTH);
+            add(LIST_CONTROLLERS, BorderLayout.CENTER);
+        }
+    };
+    
+    /**
+     * Az üzenetkijelző és üzenetküldő panel.
+     */
+    private final JPanel PANEL_MESSAGES = new JPanel() {
+        {
+            setBackground(Color.WHITE);
+            setBorder(BorderFactory.createEtchedBorder());
+        }
+    };
     
     public ChatDialog(Window owner, final ControllerWindows windows) {
         super(owner, "Chat", windows);
         setIconImage(IC_CHAT.getImage());
         
-        JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, PANEL_MESSAGES, LIST_CONTROLLERS);
+        JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, PANEL_MESSAGES, PANEL_CONTROLLERS);
         pane.setContinuousLayout(true);
-        pane.setDividerSize(2);
+        pane.setDividerSize(4);
         pane.setBorder(null);
         add(pane);
         
-        PANEL_MESSAGES.setPreferredSize(new Dimension(470, 200));
-        LIST_CONTROLLERS.setPreferredSize(new Dimension(170 - 2, 200));
+        PANEL_MESSAGES.setPreferredSize(new Dimension(490, 200));
+        LIST_CONTROLLERS.setPreferredSize(new Dimension(150 - 4, 200));
         pack();
     }
 
@@ -54,6 +99,7 @@ public class ChatDialog extends AbstractDialog {
      * Teszt.
      */
     public static void main(String[] args) {
+        UIUtil.setSystemLookAndFeel();
         JDialog d = new ChatDialog(null, null);
         d.setLocationRelativeTo(d);
         d.setVisible(true);

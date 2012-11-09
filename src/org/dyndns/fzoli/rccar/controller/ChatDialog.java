1c2b62f01060fdd754ec2c1374f2702a32658a67
zoli <zoli@NB-FZoli>
2012-11-09 16:24:02 +0100

    testing

--- src/org/dyndns/fzoli/rccar/controller/ChatDialog.java
+++ src/org/dyndns/fzoli/rccar/controller/ChatDialog.java
@@ -1,31 +1,58 @@
 package org.dyndns.fzoli.rccar.controller;
 
+import java.awt.Dimension;
 import java.awt.Window;
 import javax.swing.DefaultListModel;
 import javax.swing.JDialog;
 import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import static org.dyndns.fzoli.rccar.controller.ControllerWindows.IC_CHAT;
 import org.dyndns.fzoli.rccar.controller.ControllerWindows.WindowType;
 
 /**
- *
+ * Chatablak.
  * @author zoli
  */
 public class ChatDialog extends AbstractDialog {
 
-    private final JList<String> CONTROLLER_LIST = new JList<String>(new DefaultListModel<String>());
+    /**
+     * A vezérlők listája.
+     */
+    private final JList<String> LIST_CONTROLLERS = new JList<String>(new DefaultListModel<String>());
+    
+    /**
+     * Az üzenetkijelző és üzenetküldő panel.
+     */
+    private final JPanel PANEL_MESSAGES = new JPanel();
     
     public ChatDialog(Window owner, final ControllerWindows windows) {
         super(owner, "Chat", windows);
         setIconImage(IC_CHAT.getImage());
-        setSize(640, 200);
+        
+        JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, PANEL_MESSAGES, LIST_CONTROLLERS);
+        pane.setContinuousLayout(true);
+        pane.setDividerSize(2);
+        pane.setBorder(null);
+        add(pane);
+        
+        PANEL_MESSAGES.setPreferredSize(new Dimension(470, 200));
+        LIST_CONTROLLERS.setPreferredSize(new Dimension(170 - 2, 200));
+        pack();
     }
 
+    /**
+     * Az ablak típusát adja vissza.
+     * Az ablak azonosításához szükséges.
+     */
     @Override
     public WindowType getWindowType() {
         return WindowType.CHAT;
     }
     
+    /**
+     * Teszt.
+     */
     public static void main(String[] args) {
         JDialog d = new ChatDialog(null, null);
         d.setLocationRelativeTo(d);

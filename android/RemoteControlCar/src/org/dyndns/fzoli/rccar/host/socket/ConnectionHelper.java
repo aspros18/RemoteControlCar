b08ea6180f6f9df3ae5fe7c096c5f7ccb5b171b9
zoli <zoli@NB-FZoli>
2012-11-27 18:35:47 +0100

    android testing

--- android/RemoteControlCar/src/org/dyndns/fzoli/rccar/host/socket/ConnectionHelper.java
+++ android/RemoteControlCar/src/org/dyndns/fzoli/rccar/host/socket/ConnectionHelper.java
@@ -1,16 +1,11 @@
 package org.dyndns.fzoli.rccar.host.socket;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-
 import javax.net.ssl.SSLSocket;
 
 import org.dyndns.fzoli.rccar.ConnectionKeys;
 import org.dyndns.fzoli.rccar.clients.AbstractConnectionHelper;
 import org.dyndns.fzoli.rccar.host.ConnectionService;
 import org.dyndns.fzoli.socket.handler.AbstractSecureClientHandler;
-
-import android.util.Log;
 
 /**
  * A hoszt kliens híd szerverhez való kapcsolódását oldja meg.
@@ -23,18 +18,6 @@ public class ConnectionHelper extends AbstractConnectionHelper implements Connec
 	public ConnectionHelper(ConnectionService service) {
 		super(service.getConfig(), KEY_DEV_HOST, new int[] {KEY_CONN_DISCONNECT, KEY_CONN_MESSAGE});
 		SERVICE = service;
-	}
-	
-	@Override
-	protected SSLSocket createConnection() throws GeneralSecurityException, IOException {
-		Log.i("test", "config: " + CONFIG);
-		try {
-			return super.createConnection();
-		}
-		catch (Throwable ex) {
-			Log.e("test", "connection error", ex);
-			return null;
-		}
 	}
 	
 	@Override

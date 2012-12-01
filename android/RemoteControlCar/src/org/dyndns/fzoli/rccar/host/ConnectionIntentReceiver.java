0d05b8188b107b4bb33144938a514c7d68a3f19a
zoli <zoli@NB-FZoli>
2012-12-01 15:26:51 +0100

    android testing

--- android/RemoteControlCar/src/org/dyndns/fzoli/rccar/host/ConnectionIntentReceiver.java
+++ android/RemoteControlCar/src/org/dyndns/fzoli/rccar/host/ConnectionIntentReceiver.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 /**
  * Az alábbi rendszereseményeket továbbítja a szolgáltatásnak ha az fut, vagy el kell indítani:
  * - android.intent.action.BOOT_COMPLETED (az Android oprendszer betöltése befejeződött)
+ * - android.intent.action.MEDIA_MOUNTED (az SD-kártya felcsattolódott)
  * - android.net.conn.CONNECTIVITY_CHANGE (a hálózati kapcsolat megszünt, a telefon kapcsolódás alatt van vagy kapcsolódott egy hálózathoz)
  * - android.location.PROVIDERS_CHANGED (a GPS szenzor be- vagy ki lett kapcsolva)
  */
@@ -14,7 +15,7 @@ public class ConnectionIntentReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		if (ConnectionService.isStarted(context)) { // ha fut (vagy futott de váratlanul leállt és el kell indítani) a service
+		if (ConnectionService.isStarted(context) && !ConnectionService.isOfflineMode(context)) { // ha fut (vagy futott de váratlanul leállt és el kell indítani) a service
 			String evt = intent.getAction();
 			if (evt != null) { // ha van esemény, esemény küldése a szolgáltatásnak
 				Intent serviceIntent = new Intent(context, ConnectionService.class);

6fa52af7a3fa77a37d41f60bded5f62f09233de9
zoli <zoli@NB-FZoli>
2012-12-22 15:49:22 +0100

    window location optimizing

--- src/org/dyndns/fzoli/rccar/controller/ControllerWindows.java
+++ src/org/dyndns/fzoli/rccar/controller/ControllerWindows.java
@@ -74,6 +74,18 @@ public class ControllerWindows {
         
     });
     
+    /**
+     * Hézag az ablakok között.
+     * Az alábbi okok miatt van rá szükségem:
+     * - Windows alatt amikor az Aero téma használatban van, az ablakok elrendezése másként funkcionál, mint Aero nélkül.
+     *   Aero nélkül pixelre pontosan jó helyre kerülnek az ablakok, egyébként kicsit egymásra elcsúsznak.
+     *   Ezt az egymásra csúszást kerülöm el azzal, hogy hézagot hagyok az ablakok körül.
+     *   Ez a hézag a csetablak esetén csak a fele, hogy elkerüljem azt is, hogy a 0. magasság koordináta ne menjen a képernyő fölé,
+     *   ezzel levágva a két felső ablak címsorából egy kevesett. A csetablak mérete és a hézag felével csökkentett, így alatta is van egy kis hézag.
+     * - Linux alatt GNOME felület esetén szintén másképpen működik az ablakok pozícionálása és ezért sem lehet egyértelműen egymás mellé
+     *   tenni az ablakokat. Arról nem is beszélve, hogy XFCE alatt alapban van egy kis hézag az ablakokra hagyva.
+     * A konstans értéke: 10 pixel (optimális érték, nem ajánlatos elállítani)
+     */
     private final static int GAP = 10;
     
     /**
@@ -86,7 +98,7 @@ public class ControllerWindows {
         if (emptyHeight < 0) { // ha nem fér ki a főablak és a chatablak (a két ablak) magasságilag
             emptyHeight = SCREEN_SIZE.height - FRAME_MAIN.getHeight() - GAP; // maradék magasság csak a főablakkal
             if (emptyHeight >= DIALOG_CHAT.getMinimumSize().height) { // ha a maradék magasság elég nagy a chatablaknak
-                DIALOG_CHAT.setSize(DIALOG_CHAT.getWidth(), emptyHeight); // magasság beállítása teljes helykitöltésre
+                DIALOG_CHAT.setSize(DIALOG_CHAT.getWidth(), emptyHeight - GAP); // magasság beállítása teljes helykitöltésre
                 emptyHeight = 0; // az átméretezéssel a maradék hely pontosan nulla lett
             }
             else {
@@ -100,8 +112,8 @@ public class ControllerWindows {
         boolean isEmptyWidth = emptyWidth >= 0; // van-e szabad hely a képernyőn szélességében ...
         boolean isEmptyHeight = emptyHeight >= 0; // ... és magasságában
         
-        // főablak pozíciójának beállítása: ha a maradék szélesség negatív, szélességileg a képernyő közepére egyedül, egyébként a másik ablakkal. magasságilag ugyan ez a logika, csak a csetablakkal
-        FRAME_MAIN.setLocation((SCREEN_SIZE.width / 2) - ((FRAME_MAIN.getWidth() + (!isEmptyWidth ? 0 : BIGGER_WINDOW.getWidth() + GAP)) / 2), (SCREEN_SIZE.height / 2) - ((FRAME_MAIN.getHeight() + (!isEmptyHeight ? 0 : DIALOG_CHAT.getHeight() + GAP)) / 2));
+        // főablak pozíciójának beállítása: ha a maradék szélesség negatív, szélességileg a képernyő közepére egyedül, egyébként a másik ablakkal. magasságilag ugyan ez a logika, csak a csetablakkal és a hézag nélkül, hogy csak a fele térköz legyen
+        FRAME_MAIN.setLocation((SCREEN_SIZE.width / 2) - ((FRAME_MAIN.getWidth() + (!isEmptyWidth ? 0 : BIGGER_WINDOW.getWidth() + GAP)) / 2), (SCREEN_SIZE.height / 2) - ((FRAME_MAIN.getHeight() + (!isEmptyHeight ? 0 : DIALOG_CHAT.getHeight())) / 2));
         
         // térkép- és vezérlőablak láthatóságának beállítása: ha elférnek a főablak mellett, akkor kezdetben láthatóak, egyébként meg nem
         FRAME_MAIN.setWindowVisibility(ControllerWindows.WindowType.MAP, isEmptyWidth);
@@ -115,7 +127,7 @@ public class ControllerWindows {
         FRAME_MAIN.setWindowVisibility(ControllerWindows.WindowType.CHAT, isEmptyHeight);
         
         // chatablak pozíciójának beállítása: ha kifér a főablakkal együtt, akkor a főablak alá, egyébként a főablak aljára
-        DIALOG_CHAT.setLocation(FRAME_MAIN.getX(), FRAME_MAIN.getY() + FRAME_MAIN.getHeight() - (isEmptyHeight ? -1 * GAP : DIALOG_CHAT.getHeight()));
+        DIALOG_CHAT.setLocation(FRAME_MAIN.getX(), FRAME_MAIN.getY() + FRAME_MAIN.getHeight() - (isEmptyHeight ? -1 * GAP / 2 : DIALOG_CHAT.getHeight()));
     }
     
     /**

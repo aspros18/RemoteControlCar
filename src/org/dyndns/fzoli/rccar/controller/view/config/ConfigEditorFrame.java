041d671e3474b54854c36c5d4dfed8aee941b83e
zoli <zoli@NB-FZoli>
2013-01-02 04:40:12 +0100

    testing

--- src/org/dyndns/fzoli/rccar/controller/view/config/ConfigEditorFrame.java
+++ src/org/dyndns/fzoli/rccar/controller/view/config/ConfigEditorFrame.java
@@ -7,6 +7,8 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -287,6 +289,20 @@ public class ConfigEditorFrame extends ModalFrame {
     private void initComponents() {
         tabbedPane.setFocusable(false); // zavaró kijelölés jelzés leszedése
         tabbedPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5)); // 5 x 5 pixeles margó
+        
+        final KeyAdapter klEnter = new KeyAdapter() { // szerkeszthető mezőben enter lenyomására ugyan az történik, mint ha az OK gombra kattintanának, ha jók a beállítások
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER && !e.isAltDown() && !e.isControlDown() && !e.isShiftDown()) {
+                    if (isConfigValid()) saveConfig(); // ha a konfiguráció helyes, mentés és ablak bezárása
+                }
+            }
+
+        };
+        
+        tfAddress.addKeyListener(klEnter);
+        tfPort.addKeyListener(klEnter);
     }
     
     /**

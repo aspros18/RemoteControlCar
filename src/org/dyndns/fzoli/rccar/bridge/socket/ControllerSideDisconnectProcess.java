2aed1a11a77a138eb31fb875c2caebe99dbec649
zoli <zoli@NB-FZoli>
2013-01-22 02:27:05 +0100

    cleaning

--- src/org/dyndns/fzoli/rccar/bridge/socket/ControllerSideDisconnectProcess.java
+++ src/org/dyndns/fzoli/rccar/bridge/socket/ControllerSideDisconnectProcess.java
@@ -1,5 +1,7 @@
 package org.dyndns.fzoli.rccar.bridge.socket;
 
+import org.dyndns.fzoli.rccar.model.bridge.ControllerStorage;
+import org.dyndns.fzoli.rccar.model.bridge.StorageList;
 import org.dyndns.fzoli.socket.handler.SecureHandler;
 
 /**
@@ -10,6 +12,25 @@ public class ControllerSideDisconnectProcess extends BridgeDisconnectProcess {
 
     public ControllerSideDisconnectProcess(SecureHandler handler) {
         super(handler);
+    }
+
+    @Override
+    protected void onTimeout(Exception ex) throws Exception {
+        // TODO: jármű megállítása, ha van jármű
+        super.onTimeout(ex);
+    }
+
+    @Override
+    protected void afterTimeout() throws Exception {
+        // TODO: jármű vezérlés visszaállítása, ha azóta nem változott
+        super.afterTimeout();
+    }
+
+    @Override
+    protected void onDisconnect(Exception ex) {
+        ControllerStorage cs = StorageList.findControllerStorageByName(getRemoteCommonName());
+        if (cs != null && cs.getHostStorage() != null) cs.setHostStorage(null);
+        super.onDisconnect(ex);
     }
     
 }

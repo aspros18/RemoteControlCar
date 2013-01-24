5c8d50f025019c48e9440cf86e6ddec2574dda35
zoli <zoli@NB-FZoli>
2013-01-24 22:01:48 +0100

    note

--- src/org/dyndns/fzoli/rccar/controller/socket/ControllerDisconnectProcess.java
+++ src/org/dyndns/fzoli/rccar/controller/socket/ControllerDisconnectProcess.java
@@ -13,26 +13,46 @@ import org.dyndns.fzoli.socket.process.impl.ClientDisconnectProcess;
  */
 public class ControllerDisconnectProcess extends ClientDisconnectProcess implements ConnectionKeys {
     
+    /**
+     * Vezérlő kliens oldalra időtúllépés detektáló.
+     * 1 s és 10 s az időkorlát, 250 ms időközzel (meg kell egyezniük pontosan a szerverével).
+     * @param handler Biztonságos kapcsolatfeldolgozó, ami létrehozza ezt az adatfeldolgozót.
+     * @throws NullPointerException ha handler null
+     */
     public ControllerDisconnectProcess(SecureHandler handler) {
         super(handler, DC_TIMEOUT1, DC_TIMEOUT2, DC_DELAY); // 1 és 10 mp időtúllépés, 250 ms sleep
     }
 
+    /**
+     * Beállítja a modelben az időtúllépés értékét, ezzel frissítve a felületet is.
+     */
     private void setTimeout(boolean b) {
         ControllerModels.getData().setUnderTimeout(b);
     }
 
+    /**
+     * Ha a kapcsolat létrejött, az időtúllépés értéke hamisra állítódik be,
+     * mert előforulhat, hogy időtúllépés után megszakad a kapcsolat, és újra kapcsolódás
+     * után hívódik meg ez a metódus, amikoris a paraméter igaz.
+     */
     @Override
     protected void onConnect() {
         setTimeout(false);
         super.onConnect();
     }
     
+    /**
+     * Időtúllépés esetén a modelben frissül a timeout paraméter és ezzel frissül a felület is.
+     */
     @Override
     protected void onTimeout(Exception ex) throws Exception {
         setTimeout(true);
         super.onTimeout(ex);
     }
 
+    /**
+     * Az időtúllépés helyreállása után a modelben frissül a timeout paraméter és ezzel frissül a felület is.
+     */
     @Override
     protected void afterTimeout() throws Exception {
         setTimeout(false);

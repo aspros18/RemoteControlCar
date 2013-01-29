7a6e3e78ee1dab90fe3c67ffc145d82b6ef99dc1
zoli <zoli@NB-FZoli>
2013-01-30 00:14:14 +0100

    testing

--- src/org/dyndns/fzoli/rccar/bridge/socket/HostSideDisconnectProcess.java
+++ src/org/dyndns/fzoli/rccar/bridge/socket/HostSideDisconnectProcess.java
@@ -1,7 +1,11 @@
 package org.dyndns.fzoli.rccar.bridge.socket;
 
+import java.util.List;
+import org.dyndns.fzoli.rccar.model.bridge.ControllerStorage;
 import org.dyndns.fzoli.rccar.model.bridge.HostStorage;
 import org.dyndns.fzoli.rccar.model.bridge.StorageList;
+import org.dyndns.fzoli.rccar.model.controller.HostList;
+import org.dyndns.fzoli.rccar.model.controller.HostList.PartialHostList;
 import org.dyndns.fzoli.socket.handler.SecureHandler;
 
 /**
@@ -9,11 +13,19 @@ import org.dyndns.fzoli.socket.handler.SecureHandler;
  * @author zoli
  */
 public class HostSideDisconnectProcess extends BridgeDisconnectProcess {
-
+    
     public HostSideDisconnectProcess(SecureHandler handler) {
         super(handler);
     }
 
+    private void sendConnectionMessage(boolean connected) {
+        PartialHostList msg = new HostList.PartialHostList(getRemoteCommonName(), connected ? HostList.PartialHostList.ChangeType.ADD : HostList.PartialHostList.ChangeType.REMOVE);
+        List<ControllerStorage> controllers = StorageList.getControllerStorageList();
+        for (ControllerStorage cs : controllers) {
+            if (cs.getHostStorage() == null) cs.getMessageProcess().sendMessage(msg);
+        }
+    }
+    
     private void setTimeout(boolean b) {
         HostStorage st = StorageList.findHostStorageByName(getRemoteCommonName());
         if (st != null) st.setUnderTimeout(b);
@@ -21,7 +33,7 @@ public class HostSideDisconnectProcess extends BridgeDisconnectProcess {
     
     @Override
     protected void onConnect() {
-        setTimeout(false);
+        sendConnectionMessage(true);
         super.onConnect();
     }
 
@@ -35,6 +47,13 @@ public class HostSideDisconnectProcess extends BridgeDisconnectProcess {
     protected void afterTimeout() throws Exception {
         setTimeout(false);
         super.afterTimeout();
+    }
+
+    @Override
+    protected void onDisconnect(Exception ex) {
+        sendConnectionMessage(false);
+        setTimeout(false);
+        super.onDisconnect(ex);
     }
     
 }

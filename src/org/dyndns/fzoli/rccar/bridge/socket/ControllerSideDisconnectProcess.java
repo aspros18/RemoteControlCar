693d7586bbed9480f3cc16e94e9a5ad6efb1356d
zoli <zoli@NB-FZoli>
2013-01-31 22:01:55 +0100

    testing

--- src/org/dyndns/fzoli/rccar/bridge/socket/ControllerSideDisconnectProcess.java
+++ src/org/dyndns/fzoli/rccar/bridge/socket/ControllerSideDisconnectProcess.java
@@ -1,5 +1,6 @@
 package org.dyndns.fzoli.rccar.bridge.socket;
 
+import org.dyndns.fzoli.rccar.model.Control;
 import org.dyndns.fzoli.rccar.model.bridge.ControllerStorage;
 import org.dyndns.fzoli.rccar.model.bridge.StorageList;
 import org.dyndns.fzoli.socket.handler.SecureHandler;
@@ -10,13 +11,21 @@ import org.dyndns.fzoli.socket.handler.SecureHandler;
  */
 public class ControllerSideDisconnectProcess extends BridgeDisconnectProcess {
 
+    private ControllerStorage storage;
+    
     public ControllerSideDisconnectProcess(SecureHandler handler) {
         super(handler);
     }
 
+    private ControllerStorage getControllerStorage() {
+        if (storage != null) return storage;
+        return storage = StorageList.findControllerStorageByName(getRemoteCommonName());
+    }
+    
     @Override
     protected void onTimeout(Exception ex) throws Exception {
-        // TODO: jármű megállítása, ha van jármű
+        ControllerStorage cs = getControllerStorage();
+        if (cs != null) cs.getReceiver().setControl(new Control(0, 0));
         super.onTimeout(ex);
     }
 
@@ -28,7 +37,7 @@ public class ControllerSideDisconnectProcess extends BridgeDisconnectProcess {
 
     @Override
     protected void onDisconnect(Exception ex) {
-        ControllerStorage cs = StorageList.findControllerStorageByName(getRemoteCommonName());
+        ControllerStorage cs = getControllerStorage();
         if (cs != null && cs.getHostStorage() != null) cs.setHostStorage(null);
         super.onDisconnect(ex);
     }

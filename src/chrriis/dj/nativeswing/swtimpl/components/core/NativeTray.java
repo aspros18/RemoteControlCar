6b3e150a366084ac84ea9fa5ad132e34e44ce1eb
zoli <zoli@NB-FZoli>
2013-03-05 13:10:28 +0100

    testing

--- src/chrriis/dj/nativeswing/swtimpl/components/core/NativeTray.java
+++ src/chrriis/dj/nativeswing/swtimpl/components/core/NativeTray.java
@@ -1,40 +1,22 @@
 package chrriis.dj.nativeswing.swtimpl.components.core;
 
+import chrriis.dj.nativeswing.swtimpl.CommandMessage;
 import chrriis.dj.nativeswing.swtimpl.components.internal.INativeTray;
-import java.awt.Component;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Control;
-import chrriis.dj.nativeswing.swtimpl.core.ControlCommandMessage;
-import chrriis.dj.nativeswing.swtimpl.core.SWTNativeComponent;
-import java.util.HashMap;
 import java.util.List;
-import javax.swing.JFrame;
-import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Tray;
 import org.eclipse.swt.widgets.TrayItem;
 
 public class NativeTray implements INativeTray {
 
-    private static class TrayComponent extends SWTNativeComponent {
-
-        protected static Control createControl(Composite parent, Object[] parameters) {
-            return new Composite(parent, SWT.NONE);
-        }
-
-        protected Component createEmbeddableComponent() {
-            return super.createEmbeddableComponent(new HashMap<Object, Object>());
-        }
-
-    };
-
-    private static class CMN_create extends ControlCommandMessage {
+    private static class CMN_create extends CommandMessage {
 
         @Override
         public Object run(Object[] args) {
             byte[] imageData = (byte[]) args[0];
             String tooltip = (String) args[1];
-            Display display = getControl().getDisplay();
+            Display display = Display.getCurrent();
             NativeTrayContainer ntc = NativeTrayContainer.getInstance();
             List<TrayItem> items = ntc.getTrayItems();
             int key = items.size();
@@ -50,7 +32,7 @@ public class NativeTray implements INativeTray {
 
     }
 
-    private static class CMN_image extends ControlCommandMessage {
+    private static class CMN_image extends CommandMessage {
 
         @Override
         public Object run(Object[] args) throws Exception {
@@ -58,13 +40,13 @@ public class NativeTray implements INativeTray {
             byte[] imageData = (byte[]) args[1];
             NativeTrayContainer ntc = NativeTrayContainer.getInstance();
             TrayItem item = ntc.getTrayItems().get(key);
-            if (item != null) item.setImage(ntc.createImage(getControl().getDisplay(), imageData));
+            if (item != null) item.setImage(ntc.createImage(Display.getCurrent(), imageData));
             return null;
         }
 
     }
 
-    private static class CMN_tooltip extends ControlCommandMessage {
+    private static class CMN_tooltip extends CommandMessage {
 
         @Override
         public Object run(Object[] args) throws Exception {
@@ -78,7 +60,7 @@ public class NativeTray implements INativeTray {
 
     }
 
-    private static class CMN_dispose extends ControlCommandMessage {
+    private static class CMN_dispose extends CommandMessage {
 
         @Override
         public Object run(Object[] args) throws Exception {
@@ -88,20 +70,12 @@ public class NativeTray implements INativeTray {
 
     }
 
-    private static final TrayComponent CMP;
-
-    static {
-        CMP = new TrayComponent();
-        new JFrame().getContentPane().add(CMP.createEmbeddableComponent()); // Why is it necessary?
-        CMP.initializeNativePeer();
+    private static void asyncExec(CommandMessage msg, Object... args) {
+        msg.asyncExec(true, args);
     }
 
-    private static void asyncExec(ControlCommandMessage msg, Object... args) {
-        msg.asyncExec(CMP, args);
-    }
-
-    private static Object syncExec(ControlCommandMessage msg, Object... args) {
-        return msg.syncExec(CMP, args);
+    private static Object syncExec(CommandMessage msg, Object... args) {
+        return msg.syncExec(true, args);
     }
 
     @Override

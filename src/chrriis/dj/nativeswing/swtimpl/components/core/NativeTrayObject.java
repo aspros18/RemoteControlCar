c70fd9d4bb496b5b432b1cdf03094cc3bd36577f
zoli <zoli@NB-FZoli>
2013-03-12 03:26:01 +0100

    note

--- src/chrriis/dj/nativeswing/swtimpl/components/core/NativeTrayObject.java
+++ src/chrriis/dj/nativeswing/swtimpl/components/core/NativeTrayObject.java
@@ -1,7 +1,21 @@
+/*
+ * Christopher Deckers (chrriis@nextencia.net)
+ * http://www.nextencia.net
+ *
+ * See the file "readme.txt" for information on usage and redistribution of
+ * this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ */
 package chrriis.dj.nativeswing.swtimpl.components.core;
 
+/**
+ * An interface that every native tray object implements.
+ * @author Zolt&aacute;n Farkas
+ */
 interface NativeTrayObject {
     
+    /**
+     * Returns the unique key of the tray object.
+     */
     public int getKey();
     
 }

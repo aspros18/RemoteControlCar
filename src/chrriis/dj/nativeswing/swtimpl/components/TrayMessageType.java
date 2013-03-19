b093526cfa313c341f9c0af076616d30e089ad80
zoli <zoli@NB-FZoli>
2013-03-19 02:50:24 +0100

    note

--- src/chrriis/dj/nativeswing/swtimpl/components/TrayMessageType.java
+++ src/chrriis/dj/nativeswing/swtimpl/components/TrayMessageType.java
@@ -1,7 +1,21 @@
+/*
+ * Christopher Deckers (chrriis@nextencia.net)
+ * http://www.nextencia.net
+ *
+ * See the file "readme.txt" for information on usage and redistribution of
+ * this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ */
 package chrriis.dj.nativeswing.swtimpl.components;
 
+/**
+ * An enum indicating the message type.
+ * @author Zolt&aacute;n Farkas
+ */
 public enum TrayMessageType {
+    /** Informative message. */
     INFO,
+    /** Warning message. */
     WARNING,
+    /** Error message. */
     ERROR
 }

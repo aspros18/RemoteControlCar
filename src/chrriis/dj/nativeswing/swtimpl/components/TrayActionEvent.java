68fc56c0555e2d63d996f8cb0a421caeb86848a9
zoli <zoli@NB-FZoli>
2013-03-21 02:06:36 +0100

    note

--- src/chrriis/dj/nativeswing/swtimpl/components/TrayActionEvent.java
+++ src/chrriis/dj/nativeswing/swtimpl/components/TrayActionEvent.java
@@ -1,13 +1,36 @@
+/*
+ * Christopher Deckers (chrriis@nextencia.net)
+ * http://www.nextencia.net
+ *
+ * See the file "readme.txt" for information on usage and redistribution of
+ * this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ */
 package chrriis.dj.nativeswing.swtimpl.components;
 
+/**
+ * An event which indicates that an action occurred in a {@link JTrayObject}.
+ * @author Zolt&aacute;n Farkas
+ * @see MenuItemActionListener
+ * @see MenuItemSelectionListener
+ */
 public class TrayActionEvent<T> {
     
+    /**
+     * The object on which the event occurred.
+     */
     private final T COMPONENT;
     
+    /**
+     * Constructs an event.
+     * @param component the object on which the event occurred
+     */
     public TrayActionEvent(T component) {
        COMPONENT = component;
     }
     
+    /**
+     * The object on which the event occurred.
+     */
     public T getComponent() {
         return COMPONENT;
     }

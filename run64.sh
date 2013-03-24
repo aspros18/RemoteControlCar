28af64a90fce4c81fe17d83f011ec6be8d8e3e8b
zoli <zoli@NB-FZoli>
2013-03-24 01:20:52 +0100

    app chooser testing

--- run64.sh
+++ run64.sh
@@ -3,7 +3,7 @@
 # GNOME felületen az ilyen módon betöltött program fagyást okoz az egyik gépemen
 # a program (második) bezárásakor. Oka az, hogy az alkalmazás nem képes leállni.
 # Meghagytam magamnak ezt a scriptet, hogy azon a gépen is jól működjön a program.
-# Megjeyzés: ugyan azzal a géppel és rendszerrel, Xfce 4 felületen nincs ez a bug.
+# Megjegyzés: ugyan azzal a géppel és rendszerrel, Xfce 4 felületen nincs ez a bug.
 cd `dirname $0`
-# A kliens alkalmazást futtatja 64-bites Linux alatt.
-java -classpath desktop/BrowserTest/lib/swt/swt-linux64-4.3M5a.jar:swttest/ui.jar org.dyndns.fzoli.rccar.controller.Main
+# Az alkalmazást futtatja 64-bites Linux alatt.
+java -classpath desktop/BrowserTest/lib/swt/swt-linux64-4.3M5a.jar:swttest/ui.jar org.dyndns.fzoli.rccar.Main "$@"

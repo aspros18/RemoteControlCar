7eb81391c2839d17ab1882b0d56f59a05759abdb
zoli <zoli@NB-FZoli>
2013-03-25 03:08:00 +0100

    outer test

--- src/org/dyndns/fzoli/rccar/Main.java
+++ src/org/dyndns/fzoli/rccar/Main.java
@@ -33,13 +33,21 @@ import org.imgscalr.Scalr;
  * Ha nincs megadva paraméter, vagy az első paraméter fentiek egyikére
  * se illeszkedik, akkor a program megjeleníti az alkalmazásválasztó-ablakot.
  * Ha nincs grafikus felület, megkérdi, hogy induljon-e el a szerver alkalmazás.
+ * (Ajánlott az alkalmazás-paraméter megadása, mert ha meg van adva,
+ * gyorsabban indul az alkalmazás és a splash-screen sem tűnik el, tehát
+ * a felhasználó részletes információt kap a program betöltése közben.)
+ * Mivel mindkét alkalmazás fel van készítve a (gyorsabb, szebb) SWT alapú
+ * komponensek használatára, a program indulása előtt inicializálódik az SWT,
+ * ha az elérhető; ha nem érhető el, SWT nélkül fut tovább mindkét program.
+ * (SWT nélkül nem érhető el a natív rendszerikon és a webböngésző, ezért
+ * AWT alapú rendszerikon lesz használva és a térkép tiltás alá kerül.)
  * @author zoli
  */
 public class Main extends JFrame {
     
     /**
      * Az alkalmazás indítása előtt
-     * az alkalmazásnév és a LookAndFeel beállítódik.
+     * az alkalmazásnév és a Look and Feel beállítódik.
      */
     static {
         setApplicationName("Mobile-RC");
@@ -49,37 +57,40 @@ public class Main extends JFrame {
 
     /**
      * A felületről kiválasztott alkalmazás típusa.
+     * (<code>client</code> vagy <code>server</code>)
      */
     private String selectedApp;
     
     /**
      * Az alkalmazásválasztó-ablak konstruktora.
+     * Inicializálódik az ablak felülete, majd megjelenik az ablak.
      */
     public Main() throws HeadlessException {
-        super("Application chooser");
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setIconImage(org.dyndns.fzoli.rccar.resource.R.getImage(null, "app-chooser.png"));
+        super("Application chooser"); // címsor-szöveg beállítása konstruktor segítségével
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE); // az ablak bezárása esetén felszabadítás
+        setIconImage(org.dyndns.fzoli.rccar.resource.R.getImage(null, "app-chooser.png")); // címsor-ikon beállítása
         GridBagConstraints c = new GridBagConstraints();
-        setLayout(new GridBagLayout());
-        c.insets = new Insets(5, 5, 5, 5);
-        c.fill = GridBagConstraints.BOTH;
-        c.weightx = 1;
+        setLayout(new GridBagLayout()); // elrendezés-menedzser beállítása GBL-ra
+        c.insets = new Insets(5, 5, 5, 5); // 5x5 pixel széles margó a komponensek között
+        c.fill = GridBagConstraints.BOTH; // mindkét irányba helykitöltés engedélyezés
+        c.weightx = 1; // kezdetben csak szélességében van teljes helykitöltés
         
-        c.gridwidth = 2;
+        c.gridwidth = 2; // mivel 2 gomb van, 2 cellát foglal el az alkalmazásválasztásra felszólító szöveg
         add(new JLabel("Please select an application by clicking to one of the button.", SwingConstants.CENTER), c);
         
-        c.gridwidth = 1;
-        c.weighty = 1;
-        c.gridy = 1;
+        c.gridwidth = 1; // a gombok 1 cellát foglalnak el
+        c.weighty = 1; // hosszúságukban is kitöltve a maradék helyet
+        c.gridy = 1; // és a felszólító szöveg alá kerülnek
         
         class AppButton extends JButton { // az alkalmazásindító gombok osztálya
             
             public AppButton(String text, BufferedImage imgIcon, final String app) {
-                super(text, new ImageIcon(Scalr.resize(imgIcon, Scalr.Method.ULTRA_QUALITY, Scalr.Mode.AUTOMATIC, 64, Scalr.OP_ANTIALIAS)));
-                setHorizontalTextPosition(SwingConstants.CENTER);
-                setVerticalTextPosition(SwingConstants.BOTTOM);
+                super(text, new ImageIcon(Scalr.resize(imgIcon, Scalr.Method.ULTRA_QUALITY, Scalr.Mode.AUTOMATIC, 64, Scalr.OP_ANTIALIAS))); // 64x64 méretű kép használata
+                setHorizontalTextPosition(SwingConstants.CENTER); // a gombfelirat a gomb közepére és ...
+                setVerticalTextPosition(SwingConstants.BOTTOM); // ... a kép alá kerül
                 addActionListener(new ActionListener() {
-
+                    
+                    /** Alkalmazásválasztás esetén beállítódik a kiválasztott alkalmazás, az alkalmazásválasztó-ablak megszűnik és a main metódus folytatódik tovább. */
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         selectedApp = app;
@@ -91,23 +102,23 @@ public class Main extends JFrame {
             
         }
         
-        add(new AppButton("Controller", org.dyndns.fzoli.rccar.controller.resource.R.getIconImage(), "client"), c); // vezérlő indító gomb
+        add(new AppButton("Controller", org.dyndns.fzoli.rccar.controller.resource.R.getIconImage(), "client"), c); // a vezérlő indító gomb létrehozása
         
         c.gridx = 1;
-        add(new AppButton("Bridge", org.dyndns.fzoli.rccar.bridge.resource.R.getBridgeImage(), "server"), c); // Híd indító gomb
+        add(new AppButton("Bridge", org.dyndns.fzoli.rccar.bridge.resource.R.getBridgeImage(), "server"), c); // a Híd indító gomb létrehozása
         
-        pack();
-        setLocationRelativeTo(this);
-        setResizable(false);
-        setVisible(true);
+        pack(); // minimális méret beállítása
+        setLocationRelativeTo(this); // ablak középre helyezése
+        setResizable(false); // átméretezés tiltása
+        setVisible(true); // megjelenítés
     }
     
     /**
-     * Megadja a külső SWT jarfájl elérhetőségét.
+     * Megadja a külső SWT-jarfájl elérhetőségét.
      * Elsőként az aktuális könyvtárban keresi, ha ott nem találja, akkor az alkalmazást tartalmazó könyvtárban keres.
-     * Az SWT jarfájloknak ezen a könyvtáron belül egy <code>swt</code> nevű könyvtárban kell lenniük a következő fájlnevek egyikével:
+     * Az SWT-jarfájloknak ezen a könyvtáron belül egy <code>lib</code> nevű könyvtárban kell lenniük a következő fájlnevek egyikével:
      * swt-win32.jar; swt-win64.jar; swt-linux32.jar; swt-linux64.jar; swt-osx32.jar; swt-osx64.jar
-     * @return null, ha a fájl nem található, egyébként az OS-függő jar fájl
+     * @return null, ha a fájl nem található; egyébként a platformfüggő jarfájl útvonala
      */
     private static File getSwtJar() {
         File swtFile = null;
@@ -116,11 +127,11 @@ public class Main extends JFrame {
         String swtFileNameOsPart = osName.contains("win") ? "win" : osName.contains("mac") ? "osx" : osName.contains("linux") || osName.contains("nix") ? "linux" : null;
         if (swtFileNameOsPart != null) {
             String swtFileName = "swt-" + swtFileNameOsPart + swtFileNameArchPart + ".jar";
-            swtFile = new File("swt", swtFileName).getAbsoluteFile();
+            swtFile = new File("lib", swtFileName).getAbsoluteFile();
             if (!swtFile.exists()) {
                 try {
                     File srcFile = new File(Main.class.getProtectionDomain().getCodeSource().getLocation().toURI());
-                    swtFile = new File(new File(srcFile.getParentFile(), "swt"), swtFileName);
+                    swtFile = new File(new File(srcFile.getParentFile(), "lib"), swtFileName);
                     if (!swtFile.exists()) swtFile = null;
                 }
                 catch (Exception ex) {
@@ -132,22 +143,23 @@ public class Main extends JFrame {
     }
     
     /**
-     * Külső forrásból betölti az SWT-t, ha az még nem lett betöltve.
+     * Hozzáadja a classpath paramétereihez a platformfüggő SWT-jarfájlt, ha az nincs még hozzáadva.
+     * A jarfájlok a <code>lib</code> könyvtáron belül vannak keresve.
      * @see #getSwtJar()
      */
     private static void loadSWT() {
         boolean available;
         try {
             Class.forName("org.eclipse.swt.SWT", false, Main.class.getClassLoader());
-            available = true; // már betöltve
+            available = true; // sikerült az osztály elérése, már elérhető az SWT
         }
         catch (ClassNotFoundException ex) {
-            available = false; // még nincs betöltve
+            available = false; // még nem érhető el az SWT
         }
-        if (!available) { // ha még nincs betöltve
-            File swtJar = getSwtJar();
-            if (swtJar == null) return; // ha nincs mit betölteni, kilépés
-            try { // kísérlet a betöltésre
+        if (!available) { // SWT-jarfájl útvonalának megadása, ha még nem érhető el
+            File swtJar = getSwtJar(); // útvonal megszerzése
+            if (swtJar == null) return; // ha az SWT-jarfájl nem található, kilépés
+            try { // kísérlet a classpath kibővítésére
                 URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
                 Method method = URLClassLoader.class.getDeclaredMethod("addURL", new Class[] {URL.class});
                 method.setAccessible(true);
@@ -223,9 +235,11 @@ public class Main extends JFrame {
             while (main.isVisible()) {
                 Thread.sleep(100);
             }
+            String app = main.selectedApp; // a kiválasztott alkalmazás lekérése
+            main = null; // az ablak referenciájának megszüntetése, hogy a GC törölhesse
             // ha ki lett választva az alkalmazás, alkalmazás indítása
-            if (main.selectedApp != null) {
-                runApp(main.selectedApp, args);
+            if (app != null) {
+                runApp(app, args);
             }
         }
     }

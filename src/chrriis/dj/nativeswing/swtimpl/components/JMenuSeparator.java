555842c35a3d1e7a6df21d9df40156bb7d52a852
zoli <zoli@NB-FZoli>
2013-05-09 01:11:01 +0200

    note

--- src/chrriis/dj/nativeswing/swtimpl/components/JMenuSeparator.java
+++ src/chrriis/dj/nativeswing/swtimpl/components/JMenuSeparator.java
@@ -1,9 +1,25 @@
+/*
+ * Christopher Deckers (chrriis@nextencia.net)
+ * http://www.nextencia.net
+ *
+ * See the file "readme.txt" for information on usage and redistribution of
+ * this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ */
 package chrriis.dj.nativeswing.swtimpl.components;
 
+/**
+ * A separator line of the menu.
+ * @author Zolt&aacute;n Farkas
+ */
 public class JMenuSeparator extends JMenuBaseItem {
 
+    /**
+     * Constructs a separator line.
+     * @param parent the menu which is the parent of this tray object
+     * @param key the unique key of the tray object
+     */
     JMenuSeparator(JTrayBaseMenu parent, int key) {
         super(parent, key);
     }
-    
+
 }

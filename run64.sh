7b22d79b9611d9eb2c098c9d3b82ebe73c2f8bc1
zoli <zoli@NB-FZoli>
2013-05-26 15:28:32 +0200

    note

--- run64.sh
+++ run64.sh
@@ -4,6 +4,7 @@
 # a program (második) bezárásakor. Oka az, hogy az alkalmazás nem képes leállni.
 # Meghagytam magamnak ezt a scriptet, hogy azon a gépen is jól működjön a program.
 # Megjegyzés: ugyan azzal a géppel és rendszerrel, Xfce 4 felületen nincs ez a bug.
+# UPDATE: amióta az SWTJar helyett külső jarból töltöm be az SWT-t, a bug megszünt.
 cd `dirname $0`
 # Az alkalmazást futtatja 64-bites Linux alatt.
 java -splash:src/org/dyndns/fzoli/rccar/resource/splash.gif -classpath desktop/BrowserTest/lib/swt/swt-linux64-4.3M5a.jar:swttest/ui.jar org.dyndns.fzoli.rccar.Main "$@"

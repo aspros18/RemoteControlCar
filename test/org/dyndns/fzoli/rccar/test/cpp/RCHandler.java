15bfc63759ec2f613e1ed20a5c5654ddd5be1efc
fzoli <f.zoli@mailbox.hu>
2013-09-10 12:15:56 +0200

    C++ test

--- test/org/dyndns/fzoli/rccar/test/cpp/RCHandler.java
+++ test/org/dyndns/fzoli/rccar/test/cpp/RCHandler.java
@@ -16,10 +16,10 @@ import org.dyndns.fzoli.socket.process.SecureProcess;
  * C++ teszt része.
  * @author zoli
  */
-public class RCHandler extends AbstractSecureClientHandler {
+public class RCHandler extends AbstractSecureClientHandler implements ConnectionKeys {
 
     public RCHandler(SSLSocket socket, int connectionId) {
-        super(socket, ConnectionKeys.KEY_DEV_PURE_CONTROLLER, connectionId);
+        super(socket, KEY_DEV_PURE_CONTROLLER, connectionId);
     }
 
     @Override
@@ -31,11 +31,19 @@ public class RCHandler extends AbstractSecureClientHandler {
     @Override
     protected SecureProcess selectProcess() {
         switch (getConnectionId()) {
-            case 5:
+            case KEY_CONN_DUMMY:
                 return new RCTestProcess(this);
-            default:
+            case KEY_CONN_DISCONNECT:
                 return new ClientTestDisconnectProcess(this, 1000, 10000, 250);
+            default:
+                return null;
         }
+    }
+
+    @Override
+    protected void onProcessNull() {
+        System.out.println("null process");
+        super.onProcessNull();
     }
     
     @Override

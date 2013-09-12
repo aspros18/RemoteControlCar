2841934a6fd1171de102dac29c1968b24553fd9d
fzoli <f.zoli@mailbox.hu>
2013-09-13 00:14:52 +0200

    cleaning

--- cpp/RCServer/main.cpp
+++ cpp/RCServer/main.cpp
@@ -16,27 +16,6 @@
 #include "Config.h"
 #include "TestHandler.h"
 
-#include "JpegStreamer.h"
-#include "JpegStore.h"
-class TestListener : public JpegListener {
-    Socket* s;
-    
-    public:
-    TestListener(Socket* cs) : JpegListener("testkey") {
-        s = cs;
-    }
-    
-    void onChanged(std::string data, bool frame) {
-        if (s->isClosed()) {
-            JpegStore::removeListener(this); // TODO: a) after this call, new connections don't get onChange
-        }                                    //       b) only one connection gets events at the same time
-        std::ostream out(s->getBuffer());    //       c) optional: stop function and onChange bool return
-        std::cout << data;
-        out << data;
-    }
-    
-};
-
 SSLServerSocket *s = NULL;
 
 void exitHandler(int signal) {
@@ -58,31 +37,7 @@ void createServerSocket(Config *c) {
     sigaction(SIGTERM, &sigHandler, NULL);
 }
 
-void* runStream(void* v) {
-    Socket* sock = (Socket*) v;
-    JpegStreamer streamer("testkey");
-    streamer.start(sock);
-    pthread_exit(NULL);
-}
-
 int main(int argc, char** argv) {
-    
-    pthread_t handleThread;
-    Socket sock("gw-fzoli", 9000);
-    if (pthread_create(&handleThread, NULL,  runStream, &sock)) {
-        std::cerr << "Thread could not be created.\n";
-        sock.close();
-        exit (1);
-    }
-    ServerSocket ss(8008);
-    while (!ss.isClosed()) {
-        Socket* cs = ss.accept();
-        TestListener* listener = new TestListener(cs);
-        JpegStore::addListener(listener);
-    }
-    pthread_join(handleThread, NULL);
-    exit(0);
-    
     Config c("bridge.conf");
     if (!c.isCorrect()) {
         std::cerr << "Incorrect config file.\n";

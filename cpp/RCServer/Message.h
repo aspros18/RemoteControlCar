4f5dcc027612b210250a0322104b12201f971113
fzoli <f.zoli@mailbox.hu>
2013-09-14 21:46:32 +0200

    c++ factory test

--- cpp/RCServer/Message.h
+++ cpp/RCServer/Message.h
@@ -18,6 +18,8 @@ class Message {
         virtual std::string serialize() = 0;
         virtual void deserialize(std::string json) = 0;
         
+        virtual std::string getClassName() = 0;
+        
 };
 
 template<typename T> Message* createT() {
@@ -34,6 +36,19 @@ struct MessageFactory {
             msg_map::iterator it = getMap()->find(s);
             if (it == getMap()->end()) return NULL;
             return it->second();
+        }
+        
+        static std::string getInstanceName(Message* msg) {
+            std::string s;
+            for (msg_map::iterator i = getMap()->begin(); i != getMap()->end(); i++) {
+                Message* tmp = i->second();
+                if (tmp->getClassName() == msg->getClassName()) {
+                    s = i->first;
+                    break;
+                }
+                delete tmp;
+            }
+            return s;
         }
         
     protected:
@@ -58,7 +73,8 @@ struct MessageRegister : MessageFactory {
 };
 
 #define REGISTER_DEC_TYPE(C_NAME) \
-    static MessageRegister<C_NAME> reg
+    static MessageRegister<C_NAME> reg; \
+    std::string getClassName() { return #C_NAME; }
 
 #define REGISTER_DEF_TYPE(C_NAME, J_NAME) \
     MessageRegister<C_NAME> C_NAME::reg(#J_NAME)
